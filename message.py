5ac696a5df7a0ed78f727cb41845730711467e89
praveenddpl <34791032+praveenddpl@users.noreply.github.com>
2017-12-23 11:40:46 +0000

    Add files via upload

---
+++ message.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+
+class Msg():
+	auth_id='MAODUZYTQ0Y2FMYJBLOW'
+	auth_token='Mzk0MzU1Mzc3MTc1MTEyMGU2M2RlYTIwN2UyMzk1'
+	sub_auth_id='SAYZA2MTZJZDLJNWUXZJ'
+	sub_auth_token='NTk5NGIzYzVkZDhiNTI2NDExNGY0YzkwYWEzNTll'
+	base_url='https://api.plivo.com/v1/Account/'
+		
+	def account(self):
+		response=requests.get(self.base_url+self.auth_id, auth=(self.auth_id,self.auth_token))
+		return response.json()
+
+	def send_message(self,src,dst,text):
+		response=requests.post(self.base_url+self.auth_id+'/Message/', 
+			                   auth=(self.auth_id,self.auth_token),
+			                   json={'src':src,'dst':dst,'text':text})
+		print response.status_code
+		if response.status_code==202:
+			return response.json()
+	def msg_details(self,uuid):
+		response=requests.get(self.base_url+self.auth_id+'/Message/'+uuid,
+		                      auth=(self.auth_id,self.auth_token))
+		print response.status_code
+		return response.json()
+
+	def msg_price(self, country):
+		response = requests.get(self.base_url+self.auth_id+'/Pricing/', auth=(self.auth_id, self.auth_token), params = {'country_iso' : country} )
+		return response.json()
+
+
+
+
